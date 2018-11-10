4145f54e68d3a72253d6b26f6d080d3b3144150d
Vijay Nair <vijay@dffrnt.com>
2018-11-10 21:41:38 +0530

    Tweaked mouse follow timing and position settings
    Also added some miscellaneous updates

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
+// Bring in features from the addons listed here: https://fiplab.com/apps/infinity-dashboard-for-mac?ref=producthunt
+// Focus on UI-UX so even those without command-line fu can create custom functionality
+
 const electron = require('electron')
 const { app, BrowserWindow } = electron
+const scriptsFolder = '/Users/vijay/Projects/micestat/sc'
+const exec = require('child_process').exec
+const fs = require('fs')
+
+app.dock.hide()
   
-  function createWindow () {
+function createWindow () {
 	win = new BrowserWindow({
-		useContentSize: true,
 		alwaysOnTop: true,
 		hasShadow: false,
 		transparent: true,
@@ -11,46 +18,58 @@ const { app, BrowserWindow } = electron
 		thickFrame: false,
 		toolbar: false,
 		type: 'desktop',
- 		show: false, // disable initial window from showing
 		webPreferences: {
 			webaudio: false,
 			webgl: false,
 			textAreasAreResizable: false
 		}
 	})
-	
-	let prevMousePos = electron.screen.getCursorScreenPoint(), newMousePos, timeInactive = 0, panelVisible = false
-	setInterval(() => {
-		newMousePos = electron.screen.getCursorScreenPoint()
+
+	win.setIgnoreMouseEvents(true)
+	win.maximize()
+	win.show()
+
+	// DEBUG
+	// win.webContents.openDevTools()
+	// win.setIgnoreMouseEvents(false)
+
+
+	// run all scripts in scripts folder and push output to front-end
+	fs.readdir(scriptsFolder, (err, files) => {
+		files
+			.filter(x => !x.match('^\\.') && x.match('\\.sh$')) 				// ignore files that are either hidden or not shell scripts
+			.map(file => {
+				let params = file.split('.')
+				let refreshPeriod = params[params.length - 2]
+				let interval = 3600 * 1000
+
+				if(refreshPeriod.indexOf('h') !== -1) {
+					interval = parseInt(refreshPeriod.match('\\d+'), 10) * 3600 * 1000
+				} else if(refreshPeriod.indexOf('m') !== -1) {
+					interval = parseInt(refreshPeriod.match('\\d+'), 10) * 60 * 1000
+				}
+
+				exec(scriptsFolder + '/' + file, (err, stdout, stderr) => {
+					setTimeout(() => {
+						win.webContents.send('script_output', {txt: stdout})
+					}, 5000)
+				})
+			})
+	})
+
+	let oldPos = {x: 0, y: 0}
+	let getMousePos = () => {
+		let pos = electron.screen.getCursorScreenPoint()
 		
-		if((prevMousePos.x == newMousePos.x)) {
-			if(timeInactive > 10 && !panelVisible) {
-				win.showInactive()
-				panelVisible = true
-				
-		    	let followMouse = setInterval(() => win.setPosition(newMousePos.x + 10, newMousePos.y + 20, false), 30)
-
-		    	// hide after 10 secs
-				setTimeout(() => {
-					console.log(timeInactive)
-					if(timeInactive < 10) {
-						clearInterval(followMouse)
-
-						win.hide()
-						panelVisible = false
-					}
-				}, 10000)
-			}
-		} else {
-	    	timeInactive = 0
+		if(oldPos.x !== pos.x || oldPos.y !== pos.y) {
+			win.webContents.send('ping', {x: pos.x, y: pos.y})
+			oldPos = pos
 		}
-		
-		timeInactive++
-
-		prevMousePos = newMousePos
-	}, 1000)
+		setTimeout(getMousePos, 32)
+	}
+	getMousePos()
 
 	win.loadFile('index.html')
-  }
-  
-  app.on('ready', createWindow)
+}
+
+app.on('ready', createWindow)
